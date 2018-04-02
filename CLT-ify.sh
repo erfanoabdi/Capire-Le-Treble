08b9f3ad9582235b2eb2f969cf593d2103ac5034
Erfan Abdi <erfangplus@gmail.com>
2018-04-03 00:26:51 +0300

    CLT: set “system” label for output image
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- CLT-ify.sh
+++ CLT-ify.sh
@@ -70,7 +70,7 @@ else
 fi
 
 # Create Image
-sudo $make_ext4fs -T 0 $fcontexts -l $syssize -a system "$output" "$systemdir/"
+sudo $make_ext4fs -T 0 $fcontexts -l $syssize -L system -a system "$output" "$systemdir/"
 
 # remove tempdir
 sudo rm -rf "$tempdir"
